72bd386694f6c8bdd2561f69367096def51cc0d3
Sergio Monsalve <smonsalve@gmail.com>
2022-08-30 16:57:39 -0500

    reqs

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ pip freeze > requirements.txt
 
 
 https://realpython.com/flask-by-example-part-1-project-setup/
+https://plataforma.josedomingo.org/pledin/cursos/flask/curso/u35/
---
+++ main.py
@@ -0,0 +1,18 @@
+from decouple import config as config_decouple
+
+def create_app(enviroment):
+    app = Flask(__name__)
+
+    app.config.from_object(enviroment)
+
+    with app.app_context():
+        db.init_app(app)
+        db.create_all()
+
+    return app
+
+enviroment = config['development']
+if config_decouple('PRODUCTION', default=False):
+    enviroment = config['production']
+
+app = create_app(enviroment)
